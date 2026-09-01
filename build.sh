4af6930488ee404f113d8f55e19403e78c2d4093
tomyduch​aud2002 <tomy.windy1908@gmail.com>
2026-09-01 11:33:40 +0200

    Creation automatique du compte administrateur

--- build.sh
+++ build.sh
@@ -6,3 +6,27 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+python manage.py shell <<'PY'
+import os
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+if username and email and password:
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(
+            username=username,
+            email=email,
+            password=password
+        )
+        print("Superutilisateur créé avec succès.")
+    else:
+        print("Superutilisateur déjà présent.")
+else:
+    print("Variables du superutilisateur absentes.")
+PY
